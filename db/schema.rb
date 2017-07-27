da7b0a61a6f3fd78cbbd260c650618c819530e8d
Sebastian Wilgosz <sebastian.wilgosz@gmail.com>
2017-07-27 16:55:19 +0200

    Oktokit install & user create

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704150432) do
+ActiveRecord::Schema.define(version: 20170727145226) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "uid", null: false
+    t.string "login", null: false
+    t.string "name"
+    t.string "url"
+    t.string "avatar_url"
+    t.string "provider", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    uid "MyString"
+    login "MyString"
+    name "MyString"
+    url "MyString"
+    avatar_url "MyString"
+    provider "MyString"
+  end
+end
